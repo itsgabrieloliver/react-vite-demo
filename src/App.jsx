c3f4a4d8bb3429655ec05a45f408af056a820a19
Connor West <connorw@withnubo.com>
2024-10-29 21:24:15 -0400

    feat: Make logo glow better

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+      <div className="container">
+        <div className="imagecontainer">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logobg" alt="Vite logo"/>
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+        </div>
+        <div className="imagecontainer">
+          <a href="https://vite.dev" target="_blank">
+          <img src={reactLogo} className="logobg react" alt="React logo" />
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          </a>
+        </div>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
